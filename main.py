f28b6ab23d764a4730d12fca5d1e52ff9fb2c16b
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2025-03-24 12:49:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+import random
 window=tk.Tk()
-window.title('python')
+window.title('the numbers baseball')
 window.geometry('500x500')
 window.update_idletasks()
 window_width = window.winfo_width()
@@ -10,12 +11,25 @@ screen_height = window.winfo_screenheight()
 x_position = (screen_width - window_width) // 2
 y_position = (screen_height - window_height) // 2
 window.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+answer=random.sample(range(1,10),3)
+desc=tk.Label(window,text='1부터 9까지 3자리 숫자를 입력해주세요')
+desc.pack()
 entry=tk.Entry(window)
 entry.pack()
 def oncl():
-    label.config(text=entry.get())
+    term=[]
+    strike=0
+    ball=0
+    for digit in str(entry.get()):term.append(int(digit))
+    for i in range(len(term)):
+        if answer[i]==term[i]:strike+=1
+        elif term[i] in answer:ball+=1
+    newone=tk.Label(window,text=f'{entry.get()} {strike}s{ball}b')
+    newone.pack()
+    if strike==3:
+        winn=tk.Label(window,text='you win',font=('arial',30,'bold'),fg='red')
+        winn.pack()
     entry.delete(0,tk.END)
-    window.focus_force()
 button=tk.Button(window,text='button',command=oncl,cursor='hand2')
 button.pack()
 label=tk.Label(window)
